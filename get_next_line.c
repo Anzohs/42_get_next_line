4db4752d64a9c2397abd1de8ce14593b5e99f30a
Hugo Emanuel Ladeiro Nobre <hladeiro@c1r10s1.42lisboa.com>
2024-05-07 19:03:26 +0100

    adding new version

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hladeiro <hladeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 14:40:09 by hladeiro          #+#    #+#             */
-/*   Updated: 2024/05/03 20:14:07 by hladeiro         ###   ########.fr       */
+/*   Updated: 2024/05/07 18:59:39 by hladeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,11 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = malloc(BUFFER_SIZE);
-	if(!str)
-		return (NULL);
-	list = ft_new_list(str);
-	if(!list)
-		return (NULL);
-	read(fd,list->content, BUFFER_SIZE);
-
-	printf("%s", list->content);
-	free_tlist(list);
-	return (NULL);
+	
+	ft_add_str(fd);
+	str = ft_string(&list);
+	free_tlist(&list);
+	return (str);
 }
 
 int	main()
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,32 +7,142 @@
 /*   By: hladeiro <hladeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:00:49 by hladeiro          #+#    #+#             */
-/*   Updated: 2024/05/03 20:17:25 by hladeiro         ###   ########.fr       */
+/*   Updated: 2024/05/07 18:25:56 by hladeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+char copy_str(char *p, t_list *list, int num)
+{
+	int	i;
+	int	j;
+	i = 0;
+	while (i < num)
+	{
+		while (list->content[j])
+			p[i++] = list->content[j++]
+		j = 0;
+	}
+	p[i] = 0;
+	return (p);
+}
+
+
+char  ft_string(t_list **list)
+{
+	t_list *new;
+	int	i;
+	int z;
+	char *str;
+
+	new = *list;
+	i = 0;
+	z = 0;
+	while (new->next)
+	{
+		i += (new->content);
+		new = new->next;
+	}
+	str = malloc(i+1);
+	if (!str)
+		return (NULL);
+	new = list;
+	str
+}
+
+int	ft_length(char *content)
+{
+	int	i;
+
+	i = 0;
+	while (content[i] && content[i] != '/n')
+		i++;
+	return (i);
+}
+
+
+void	ft_list_last(t_list *list)
+{
+	if (!list)
+		return (NULL);
+	while (list->next)
+		lst = list->next;
+	return (lst);
+	
+}
+
+void	ft_add_back(t_list **list, t_list *lista)
+{
+	t_list	*new;
+
+	new = ft_list_last(*list);
+	if (!*list)
+		*list = lista;
+	else
+		mew->next = lista;
+
+}
+
+int ft_brk(char *p)
+{
+    int i;
+
+	if (!p)
+		return (0);
+    while (str[i] != '\n' && str[i])
+            i++;
+    return(str[i] == '\n');
+}
+
+void    ft_add_str(static t_list *list, int fd);
+{
+    char    *str;
+    int	i;
+	t_list	*new;
+
+	new = list;
+    while(!ft_brk(new->content))
+    {
+        str = malloc(BUFFER_SIZE + 1)
+		if (!str)
+		{
+			free_tlist(list);
+			return; 
+		}
+		i = read(fd, str, BUFFER_SIZE);
+		str[i] = 0;
+		if(!new)
+			ft_add_back(&list, ft_new_list(str))
+			new = ft_last(&list);
+		else
+		{	
+			ft_add_back(&list, ft_new_list(str))
+			new = ft_list_last(&list);
+		}
+		free(str);
+    }
+}
 
 t_list  *ft_new_list(char *content)
 {
     t_list  *new;
-
+    
     new = malloc(sizeof(t_list));
     if (!new)
         return (NULL);
-    new->content = content;
     new->next = NULL;
     return new;
-
 }
 
-void	free_tlist(t_list	*list)
+void	free_tlist(static t_list **list)
 {
 	t_list	*t_next;
-	while(list-> next)
+
+	while(*list-> next)
 	{	
-		t_next = list->next;
-		free(list);
+		t_next = *list->next;
+		free(*list);
 		list =  t_next;
 	}
+	free(*list);
 }
